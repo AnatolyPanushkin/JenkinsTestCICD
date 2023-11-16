e45d4e600093cf8bce3b31f65cbc503e79f11833
Anatoly <anatolypanushkin@mail.ru>
2023-11-16 15:44:04 +0300

    fix for test

--- WebApplication3/Controllers/WeatherForecastController.cs
+++ WebApplication3/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "snneezing", "racing", "hilly", "Coml", "Milm", "Warmmm", "Bmmalmy", "Hommt", "Swelmmtering", "Scorchmming"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
